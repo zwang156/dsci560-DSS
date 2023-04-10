e6b252f948bba5a8edac04a0958a8f4293871048
Heisenbug <58136242+HeisenbugZ@users.noreply.github.com>
2023-04-10 15:36:31 -0700

    Dynamic Chart

--- front-end/src/components/LineChart.js
+++ front-end/src/components/LineChart.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import ReactEcharts from "echarts-for-react"
 import '../styles/Sider.css'
 
-const options = {
+const DEFAULT = {
   grid: { top: 20, right: 40, bottom: 20, left: 40 },
   xAxis: {
     type: "category",
@@ -24,11 +24,35 @@ const options = {
 }
 
 function LineChart({ district }) {
+  const [option, setOption] = useState(DEFAULT);
+
+  useEffect(() => {
+    if (district !== "LA"){
+      const newOption = {
+        grid: { top: 20, right: 40, bottom: 20, left: 40 },
+        xAxis: {
+          type: "category",
+          data: ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            data: [100*parseInt(district), 300, 350, 200*parseInt(district), 280],
+            type: "line",
+            smooth: true
+          }
+        ]
+      }
+      setOption(newOption)
+    }
+  }, [district])
   return (
     <div className='lineChart'>
       {/* <h3>Line Chart for {`District ${district}`}</h3> */}
       <ReactEcharts
-        option={options}
+        option={option}
         style={{ width: "100%", height: "100%" }}
       ></ReactEcharts>
     </div>
--- front-end/src/components/PieChart.js
+++ front-end/src/components/PieChart.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import ReactEcharts from "echarts-for-react"
 import '../styles/Sider.css'
-const options = {
+const DEFAULT = {
   grid: { top: 20, right: 40, bottom: 20, left: 40 },
   xAxis: {
     show:  false,
@@ -17,17 +17,42 @@ const options = {
       type: "pie",
       smooth: true
     }
-  ],
-  tooltip: {
-    trigger: "axis"
-  }
+  ]
 }
+
 function PieChart({ district }) {
+  const [option, setOption] = useState(DEFAULT);
+  useEffect(() => {
+    if (district !== "LA"){
+      const newOption = {
+        grid: { top: 20, right: 40, bottom: 20, left: 40 },
+        xAxis: {
+          type: "category",
+          data: ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            data: [100*parseInt(district), 300, 350, 200, 280],
+            type: "pie",
+            smooth: true
+          }
+        ]
+      }
+      setOption(newOption)
+    }
+  }, [district])
+  
+  
+  
+  
   return (
     <div className='pieChart'>
       {/* <h3>Pie Chart for {`District ${district}`}</h3> */}
       <ReactEcharts
-        option={options}
+        option={option}
         style={{ width: "100%", height: "100%" }}
       ></ReactEcharts>
     </div>
