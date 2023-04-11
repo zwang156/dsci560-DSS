421dc42e8bec1d1d7ea80ee0cf81ac427d69e362
czq01 <czq0106@outlook.com>
2023-04-11 14:39:37 -0700

    Reconstruct: Fake Data modify

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import json
 import flask
-from flask import Flask,  request
+from flask import Flask, request
 import os
 import sys
 from query_handler import *
@@ -19,93 +19,99 @@ db = DataBase(**login_info)
 
 # define api nodes
 
+
 @app.route("/")
 def root():
     return "<div> Root Page </div>"
 
-@app.route("/test") #accept: {**kwargs: Any}
+
+@app.route("/test")  # accept: {**kwargs: Any}
 def test():
     sql = test_sql(request.query_string)
-    data = db.query(sql);
+    data = db.query(sql)
     return data.json()
 
-@app.route("/description") #accept: {code: int}
+
+@app.route("/description")  # accept: {code: int}
 def description():
     sql = description_sql(request.query_string)
-    data = db.query(sql);
+    data = db.query(sql)
     return data.json()
 
-@app.route("/recommandation") #accept: {district: int}
+
+@app.route("/recommandation")  # accept: {district: int}
 def recommandation():
     sqls = recommendation_sql(request.query_string)
-    data0 = db.query(sqls[0]);
-    data1 = db.query(sqls[1]);
+    data0 = db.query(sqls[0])
+    data1 = db.query(sqls[1])
     fake_data = {
         "recommandations": [
-            {"name": "auto",
-             "code": "5411",
-             "rank": 1},
-            {"name": "auto",
-             "code": "33234",
-             "rank": 2},
-            {"name": "auto",
-             "code": "7657",
-             "rank": 3},
-            {"name": "auto",
-             "code": "5411",
-             "rank": 4},
-            {"name": "auto",
-             "code": "796",
-             "rank": 5},
+            {"name": "Legal services", "code": "5411", "rank": 1},
+            {"name": "Full-service restaurants", "code": "722511", "rank": 2},
+            {"name": "Educational services", "code": "611", "rank": 3},
+            {"name": "Offices of physicians", "code": "621111", "rank": 4},
+            {"name": "Advertising & related services", "code": "5418", "rank": 5},
         ]
     }
     return json.dumps(fake_data)
-    return f"[{data0},{data1}]"
 
-@app.route("/active_data") #accept: {district: int}
+
+@app.route("/active_data")  # accept: {district: int}
 def trend():
     sql = "select industry_code, date, active_count from industry order by date, active_count where district="
     data = db.query(sql)
-    return json.dumps({
-        "time": ["2015/01", "2015/02", "2015/03", "2015/04", "2016/01", "2016/02"],
-        "industris": [
-            {"name": "auto",
-             "code": "5311",
-             "data": [1234, 1235, 2935, 1928, 1203, 1290]},
-            {"name": "auto",
-             "code": "5411",
-             "data": [1234, 1200, 1150, 1400, 1202, 1290]},
-            {"name": "auto",
-             "code": "5411",
-             "data": [2345,2456,2567,2134,2432,2222]},
-        ]
-    })
+    return json.dumps(
+        {
+            "time": ["2015/01", "2015/02", "2015/03", "2015/04", "2016/01", "2016/02"],
+            "industris": [
+                {"name": "Lessors of Real Estate",
+                 "code": "5311",
+                 "data": [1234, 1235, 2935, 1928, 1203, 1290],
+                },
+                {"name": "Legal services",
+                 "code": "5411",
+                 "data": [1234, 1200, 1150, 1400, 1202, 1290],
+                },
+                {
+                    "name": "Full-service restaurants",
+                    "code": "722511",
+                    "data": [2345, 2456, 2567, 2134, 2432, 2222],
+                },
+            ],
+        }
+    )
 
-@app.route("/change_ratio") #accept: {district: int}
+
+@app.route("/change_ratio")  # accept: {district: int}
 def ratio():
-    return json.dumps({
-        "time": ["2015/01", "2015/02", "2015/03", "2015/04", "2016/01", "2016/02"],
-        "industris": [
-            {"name": "auto",
-             "code": "5311",
-             "data": [0.12, 0.05, -0.1, 0.1, 0.15, 0.03]},
-            {"name": "auto",
-             "code": "5411",
-             "data": [0.04, 0.13, 0.01, -0.2, 0.1, 0.15]},
-            {"name": "auto",
-             "code": "5411",
-             "data": [0,0,0.02,0.03,-.01,-.5]},
-        ]
-    })
-    return json.dumps({
-        "time": ["2015/01", "2015/02", "2015/03", "2015/04", "2016/01", "2016/02"],
-        "5311": [0.12, 0.05, -0.1, 0.1, 0.15, 0.03],
-        "5411": [0.04, 0.13, 0.01, -0.2, 0.1, 0.15],
-        "722511": [0,0,0.02,0.03,-.01,-.5],
-    })
+    return json.dumps(
+        {
+            "time": ["2015/01", "2015/02", "2015/03", "2015/04", "2016/01", "2016/02"],
+            "industris": [
+                {
+                    "name": "Lessor of Real Estate",
+                    "code": "5311",
+                    "data": [0.12, 0.05, -0.1, 0.1, 0.15, 0.03],
+                },
+                {
+                    "name": "Legal Services",
+                    "code": "5411",
+                    "data": [0.04, 0.13, 0.01, -0.2, 0.1, 0.15],
+                },
+                {
+                    "name": "Full-service restaurants",
+                    "code": "722511",
+                    "data": [0, 0, 0.02, 0.03, -0.01, -0.5],
+                },
+            ],
+        }
+    )
 
-@app.route("/anynode") #accept: {**kwargs: any}
-def node(): ...
+
+@app.route("/anynode")  # accept: {**kwargs: any}
+def node():
+    ...
+
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
